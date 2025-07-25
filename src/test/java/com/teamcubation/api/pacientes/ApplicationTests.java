caef2dc5984c6f20708c2748d98826703fac1f4b
gaston-areco <50884317+gaston-areco@users.noreply.github.com>
2025-07-24 23:43:15 -0300

    Update ApplicationTests.java

--- src/test/java/com/teamcubation/api/pacientes/ApplicationTests.java
+++ src/test/java/com/teamcubation/api/pacientes/ApplicationTests.java
@@ -2,13 +2,15 @@ package com.teamcubation.api.pacientes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ApplicationTests {
   @Test
   public void dummyTest() {
     assertTrue(true);
   }
-
 }
